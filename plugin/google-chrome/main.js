6a0e773b31500fc66ae4cd824b04d83b11004cd6
nAndy <nandy@wikia-inc.com>
2015-05-17 23:16:11 +0200

    ported functionality from firefox - clicking extension icon takes you to TCL homepage

--- plugin/google-chrome/main.js
+++ plugin/google-chrome/main.js
@@ -1,7 +1,16 @@
-var id,
+var characterName = '',
+    menuItem,
+    tibiaCharacterLookupHomepageUrl = 'http://lukaszewski.it/tibia-character-lookup/',
     tibiaCharacterLookupUrl = 'https://secure.tibia.com/community/?subtopic=characters&name=',
-    characterName = '',
     ga = ga || function() {};
+
+chrome.browserAction.onClicked.addListener(handleHomepageButtonClick);
+
+menuItem = chrome.contextMenus.create({
+  "title": "Lookup Tibia character...",
+  "contexts": ["selection"],
+  "onclick": lookupCharacter
+});
 
 function lookupCharacter(info, tab) {
   ga('send', 'event', 'context-menu-item', 'click', 'tibia-com-search');
@@ -12,8 +21,8 @@ function lookupCharacter(info, tab) {
   });
 }
 
-id = chrome.contextMenus.create({
-  "title": "Lookup Tibia character...",
-  "contexts": ["selection"],
-  "onclick": lookupCharacter
-});
+function handleHomepageButtonClick() {
+  chrome.tabs.create({
+    url: tibiaCharacterLookupHomepageUrl
+  });
+}
