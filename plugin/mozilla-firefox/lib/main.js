fcac169778f3927c996341e3aff1330571f2e46d
nAndy <nandy@wikia-inc.com>
2015-05-19 22:53:51 +0200

    added my own analytics tracker for firefox

--- plugin/mozilla-firefox/lib/main.js
+++ plugin/mozilla-firefox/lib/main.js
@@ -5,7 +5,8 @@ var buttons = require('sdk/ui/button/action'),
     characterName = '',
     menuItem,
     tibiaCharacterLookupHomepageUrl = 'http://lukaszewski.it/tibia-character-lookup/',
-    tibiaCharacterLookupUrl = 'https://secure.tibia.com/community/?subtopic=characters&name=';
+    tibiaCharacterLookupUrl = 'https://secure.tibia.com/community/?subtopic=characters&name=',
+    analyticsUrl = 'http://lukaszewski.it/tibia-character-lookup/analytics.php';
 
 button = buttons.ActionButton({
   id: "tibia-character-lookup-link",
@@ -28,11 +29,38 @@ menuItem = contextMenu.Item({
   onMessage: lookupCharacter
 });
 
+function ga() {
+  var Request = require("sdk/request").Request,
+      command = arguments[0] || false,
+      action = arguments[1] || false,
+      analyticsObject = {};
+
+  if(command === 'send' && action === 'event') {
+    analyticsObject.category = arguments[2];
+    analyticsObject.action = arguments[3];
+
+    if(arguments[4]) {
+      analyticsObject.label = arguments[4];
+    }
+
+    try {
+      Request({
+        url: analyticsUrl,
+        content: 'data=' + JSON.stringify(analyticsObject)
+      }).post();
+    } catch(e) {
+      // don't care; just don't block other features of add-on
+    }
+  }
+}
+
 function lookupCharacter(selectedText) {
+  ga('send', 'event', 'firefox/context-menu-item', 'click', 'tibia-com-search');
   characterName = selectedText;
   tabs.open(tibiaCharacterLookupUrl + characterName);
 }
 
 function handleHomepageButtonClick() {
+  ga('send', 'event', 'firefox/homepage-button', 'click');
   tabs.open(tibiaCharacterLookupHomepageUrl);
 }
